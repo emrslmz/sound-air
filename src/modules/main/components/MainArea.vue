1a0beb7478f0ee35789e6b61da8e0d6f40fed0cd
Emre Solmaz <emrslmz57@gmail.com>
2021-02-04 12:52:25 +0300

    adding sidebar and footer

--- src/modules/main/components/MainArea.vue
+++ src/modules/main/components/MainArea.vue
@@ -1,14 +1,39 @@
 <template>
-<div>
+<div class="main-top pt-5">
   <div class="container">
-    <h1 class="text-red">
+   <div class="main-top-div">
+     <h1>Sound AIR for everyone</h1>
+     <p>and all sounds are free</p>
+   </div>
+   <div style="padding-top: 100px"></div>
+    <div class="row d-flex">
+      <div class="col-12 col-md-6">
+      </div>
+      <div class="col-12 col-md-6">
+        <h4>Choose the playlist that suits you best</h4>
+      </div>
 
-    </h1>
+    </div>
   </div>
 </div>
 </template>
 
 <style scoped>
-.container {
+.main-top {
+  padding-left: 300px;
+}
+
+@media (max-width: 768px) {
+  .main-top {
+    display: inline;
+    position: relative;
+  }
+}
+
+.main-top-div h1 {
+  font-weight: bold;
+}
+.main-top-div p {
+  letter-spacing: 5px;                                                
 }
 </style>
