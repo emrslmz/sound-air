01b996db5b893c0e5926dbfb400502d90ba17345
Emre Solmaz <emrslmz57@gmail.com>
2021-02-05 16:05:05 +0300

    adding danger and success card for contact us

--- src/modules/main/components/assets/stable/News.vue
+++ src/modules/main/components/assets/stable/News.vue
@@ -7,31 +7,61 @@
        <p class="text-spacing5">your feedback is valuable to us.</p>
      </div>
 
-     <div class="card" style="width: 18rem;">
-       <div class="not-found-images"></div>
-       <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="#" class="btn btn-primary">Go somewhere</a>
-       </div>
-     </div>
+      <div class="d-flex row">
+        <div class="col-12 col-md-4">
 
+        <!--SMALL PHOTOS AREA-->
+          <div class="card">
+            <div class="not-found-images" style="background-image: url('/images/img/image-not-found.png')"></div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        <!--/SMALL PHOTOS AREA-->
+
+        </div>
+        <div class="col-12 col-md-8">
+
+        <!--BIG PHOTOS AREA-->
+          <div class="card">
+            <div class="not-found-images" style="background-image: url('/images/img/image-not-found.png')"></div>
+            <div class="card-body" >
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        <!--/BIG PHOTOS AREA-->
+
+        </div>
+
+      </div>
    </div>
   </div>
 </template>
 
 
 <style scoped>
+
 .last-updates-padding {
   padding-top: 100px;
 }
 
 .not-found-images {
-  background-image: url("/images/img/image-not-found.png");
+  border-radius: 20px 20px 0 0;
+  background-position: center;
+  /*background-attachment: fixed;*/
   background-repeat: no-repeat;
-  background-size: 300px;
-  width: 300px;
+  background-size: 800px;
+  min-width: 300px;
   height: 200px;
+}
+
+.card {
+  border-radius: 20px;
+  width: 100%;
+  min-height: 200px;
+  box-shadow: 0 0 10px 2px #cecece;
 }
 
 </style>
