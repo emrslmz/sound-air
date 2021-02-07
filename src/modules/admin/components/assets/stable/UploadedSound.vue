9185540fef4893291e734f3cfc8e3f8c9ca2147f
Emre Solmaz <emrslmz57@gmail.com>
2021-02-07 10:14:05 +0300

    adding upload sound from admin panel

--- src/modules/admin/components/assets/stable/UploadedSound.vue
+++ src/modules/admin/components/assets/stable/UploadedSound.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="text-center pt-5">
-      <div class="uploaded-sound-card">
+      <div class="container">
+           <h3 class="text-white">Uploaded Sound</h3>
+        <div class="uploaded-sound-card">
+            <div class="d-flex align-items-center py-3">
+              <div class="col-12 col-md-2 uploaded-sound-card-left">
+                <h6> <i class="fab fa-algolia"></i></h6>
+                <h6><button class="btn btn-primary btn-sm">Edit/Delete</button></h6>
+              </div>
+              <div class="col-12 col-md-10 text-left uploaded-sound-card-right">
+                <h6><i class="fas fa-file-signature"></i> Sound Name:</h6>
+                <h6><i class="far fa-file-audio"></i> Audio Name:</h6>
+              </div>
+            </div>
+        </div>
 
       </div>
   </div>
@@ -12,5 +25,18 @@
   background-color: white;
   min-width: 200px;
   min-height: 100px;
+}
+
+.uploaded-sound-card-left i {
+  font-size: 36px;
+  color: #191919;
+}
+.uploaded-sound-card-left button {
+  border-radius: 10px;
+}
+
+.uploaded-sound-card-right h6 {
+  font-weight: bold;
+  
 }
 </style>
