8f496247c67b6ee2b265d6a92e2b5201cd67f737
Emre Solmaz <emrslmz57@gmail.com>
2021-02-11 19:34:59 +0300

    adding timer v1

--- src/modules/main/components/assets/stable/AccordingWeather.vue
+++ src/modules/main/components/assets/stable/AccordingWeather.vue
@@ -1,19 +1,51 @@
 <template>
-<div>
   <div>
-    <iframe src="https://www.meteoblue.com/tr/hava/widget/daily?geoloc=detect&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=monochrome&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&windspeed=0&windgust=0&winddirection=0&uv=0&humidity=0&precipitation=0&precipitationprobability=0&precipitationprobability=1&spot=0&pressure=0&layout=light"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 216px; height: 420px"></iframe>
+   <div class="text-center pt-5">
+     <h1>Weather</h1>
+     <div>
+       <h6>
+         <span v-if="timer.minutes < 10">0{{ timer.minutes }}</span><span v-else>{{ timer.minutes }}</span>
+         :
+        <span v-if="timer.seconds < 10">0{{ timer.seconds }}</span><span v-else>{{timer.seconds}}</span>
+       </h6>
+       <button class="btn btn-dark btn-sm mx-1" @click="increamentTime()">Zamanı arttır</button>
+       <button class="btn btn-dark btn-sm mx-1" @click="decreamentTime()">Zamanı Azalt</button>
+     </div>
+     <div class="py-3">
+
+       <button class="btn btn-danger" @click="startTimer()">Başlat</button>
+     </div>
+   </div>
   </div>
-
-  <div>
-    <iframe src="https://www.meteoblue.com/tr/hava/maps/widget?windAnimation=0&windAnimation=1&gust=0&satellite=0&satellite=1&coronaWeatherScore=0&geoloc=detect&tempunit=C&windunit=km%252Fh&lengthunit=metric&zoom=5&autowidth=auto"  frameborder="0" scrolling="NO"  sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 100%; height: 720px"></iframe>
-  </div>
-</div>
-
-
-<!--  <div>-->
-<!--    <iframe src="https://www.meteoblue.com/tr/hava/widget/daily/%c4%b0stanbul_t%c3%bcrkiye_745044?geoloc=fixed&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&winddirection=1&uv=0&humidity=0&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&spot=1&pressure=0&layout=light"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 216px; height: 420px"></iframe><div>&lt;!&ndash; DO NOT REMOVE THIS LINK &ndash;&gt;<a href="https://www.meteoblue.com/tr/hava/hafta/%c4%b0stanbul_t%c3%bcrkiye_745044?utm_source=weather_widget&utm_medium=linkus&utm_content=daily&utm_campaign=Weather%2BWidget" target="_blank">meteoblue</a></div>-->
-<!--  </div>-->
 </template>
+
+<script>
+export default {
+  name: 'Weather',
+  data() {
+    return {
+      timer: {
+        minutes: 0,
+        seconds: 0,
+      }
+    }
+  },
+  methods: {
+    increamentTime() {
+      this.timer += 5;
+    },
+    decreamentTime() {
+      this.timer -= 5;
+    },
+    startTimer() {
+      setInterval(() => {
+        this.timer.minutes--;
+      },1000);
+    }
+  },
+
+}
+</script>
 
 
 <style scoped>
